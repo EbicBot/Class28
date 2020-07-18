2dbcb5e22cf7195972bcc6db615cc042000debe8
Varun <vjayaram2006@gmail.com>
2020-07-18 11:40:08 -0400

    commit on master

---
+++ Slingshot.js
@@ -0,0 +1,20 @@
+class slingShot{
+    constructor(A,B){
+        var options = {
+            bodyA: A,
+            pointB: B,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
